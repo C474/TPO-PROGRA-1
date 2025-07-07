a323d6d622388ad7b5c2a9d68676f63787cab258
TobiasValdez <tobias_trabajo@hotmail.com>
2025-07-07 16:23:05 -0300

    Agregado de bloques protegidos

--- guardado.py
+++ guardado.py
@@ -14,19 +14,26 @@ def guardar_partida(nombre_partida, tablero, ayuda, contador):
     partidas = []
 
     if os.path.exists(NOMBRE_ARCHIVO_PARTIDAS):
-        with open(NOMBRE_ARCHIVO_PARTIDAS, "r") as archivo:
-            try:
+        try:
+            with open(NOMBRE_ARCHIVO_PARTIDAS, "r") as archivo:
                 partidas = json.load(archivo)
-            except json.JSONDecodeError:
+        except json.JSONDecodeError:
                 # El archivo existe pero está vacío o corrupto: empieza con lista vacía
                 partidas = []
+        except Exception as e:
+            print(f"No se pudo leer el archivo: {e}")
+            partidas = []
+
 
     # Reemplaza si ya existe
     partidas = [p for p in partidas if p["nombre"] != nombre_partida]
     partidas.append(partida)
 
-    with open(NOMBRE_ARCHIVO_PARTIDAS, "w") as archivo:
-        json.dump(partidas, archivo, indent=4)
+    try:
+        with open(NOMBRE_ARCHIVO_PARTIDAS, "w") as archivo:
+            json.dump(partidas, archivo, indent=4)
+    except Exception as e:
+        print(f"No se pudo escribir el archivo: {e}")
 
     # print(f"✅ Partida '{nombre_partida}' guardada automáticamente.")
 
@@ -34,9 +41,11 @@ def cargar_partida(nombre_partida):
     if not os.path.exists(NOMBRE_ARCHIVO_PARTIDAS):
         print("No hay partidas guardadas.")
         return None
-
-    with open(NOMBRE_ARCHIVO_PARTIDAS, "r") as archivo:
-        partidas = json.load(archivo)
+    try:
+        with open(NOMBRE_ARCHIVO_PARTIDAS, "r") as archivo:
+            partidas = json.load(archivo)
+    except Exception as e:
+        print(f"No se pudo leer el archivo: {e}")
 
     for partida in partidas:
         if partida["nombre"] == nombre_partida:
